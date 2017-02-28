b38601a69aee31795e28a6c9d769b5eb6daa418a
ZongyaoZhou <18482195579@163.com>
2017-02-28 15:39:26 +0800

    合并banner

--- app/src/main/java/com/example/administrator/xiangou/main/MainActivity.java
+++ app/src/main/java/com/example/administrator/xiangou/main/MainActivity.java
@@ -2,14 +2,59 @@ package com.example.administrator.xiangou.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.administrator.xiangou.R;
+import com.example.administrator.xiangou.tool.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> imgUrls,titles;
+    private Banner mBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBanner();
+    }
+
+    private void initBanner() {
+        imgUrls = new ArrayList<>();
+        imgUrls.add("http://dynamic-image.yesky.com/740x-/uploadImages/2017/052/41/18XAS82RL2O1_800x1200.jpg");
+        imgUrls.add("http://dynamic-image.yesky.com/740x-/uploadImages/2017/049/27/9SI697LWZF0E_800x1200.jpg");
+        imgUrls.add("http://dynamic-image.yesky.com/740x-/uploadImages/2017/051/09/S7R05BP4N4X0_800x1200.jpg");
+        imgUrls.add("http://dynamic-image.yesky.com/740x-/uploadImages/2017/049/00/61XX007XTFBW_800x1200.jpg");
+        imgUrls.add("http://dynamic-image.yesky.com/740x-/uploadImages/2017/049/21/C7UE22883BE7_800x1200.jpg");
+        titles = new ArrayList<>();
+        titles.add("美女1");
+        titles.add("美女2");
+        titles.add("美女3");
+        titles.add("美女4");
+        titles.add("美女5");
+
+        mBanner = (Banner) findViewById(R.id.banner_home).findViewById(R.id.banner);
+        //设置banner样式
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置指示器位置（当banner模式中有指示器时）
+        mBanner.setIndicatorGravity(BannerConfig.CENTER);
+        //设置图片加载器
+        mBanner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        mBanner.setImages(imgUrls);
+        //设置banner动画效果
+        mBanner.setBannerAnimation(Transformer.DepthPage);
+        //设置轮播时间
+        mBanner.setDelayTime(3000);
+        //设置自动轮播
+        mBanner.isAutoPlay(true);
+//        //设置标题集合（当banner样式有显示title时）
+        mBanner.setBannerTitles(titles);
+        mBanner.start();
     }
 }
